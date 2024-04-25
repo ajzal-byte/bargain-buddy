37421af84c89eb90755606edfd02c1b32ef3125c
ajzal-byte <ajzalaju10@gmail.com>
2024-04-25 16:52:36 +0530

    typo fixed

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ const Home = async () => {
       </section>
 
       <section className="trending-section">
-        <h2 className="section-text">Trending</h2>
+        <h2 className="section-text">Trending Products</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {allProducts?.map((product) => (
