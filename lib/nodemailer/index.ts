32828a5ce2076a9db6eb8a9ff550a70fef7f2ef4
ajzal-byte <ajzalaju10@gmail.com>
2024-04-26 12:45:23 +0530

    outlook -> gmail

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -81,14 +81,14 @@ export const generateEmailBody = (
 };
 
 const transporter = nodemailer.createTransport({
-  pool: true,
-  service: "hotmail",
-  port: 2255,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
-  maxConnections: 1,
 });
 
 export const sendEmail = async (
