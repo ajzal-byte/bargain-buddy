554c326fbe6a727899000d943e59603c9f8e8b39
ajzal-byte <ajzalaju10@gmail.com>
2024-06-09 12:06:37 +0530

    Revert "next downgraded to 13.5"
    This reverts commit 16b2f9e9b60ea446f9c1b65a09fe40279baf4b44.

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
